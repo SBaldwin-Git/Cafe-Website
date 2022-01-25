902340c51acb39657d6fdec3b5b70d5917231358
Sam Baldwin <sambaldwin92@gmail.com>
2022-01-25 17:48:21 +0000

    Background image added

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('Hi Sam!');
+import './style.css';
+
+const bodyContainer = document.querySelector('body');
+const contentContainer = document.querySelector('#content');
+
+let shopHeader = document.createElement('h1');
+shopHeader.textContent = 'Cafe Naples';
+
+contentContainer.appendChild(shopHeader);
