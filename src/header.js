73ef19cc729fb9c2e847d82fab8d7aa42ddcc5b3
Sam Baldwin <sambaldwin92@gmail.com>
2022-01-30 17:03:35 +0000

    navbar created and styled

--- src/header.js
+++ src/header.js
@@ -3,15 +3,32 @@ function createHeader() {
     const header = document.createElement('header');
     const shopHeader = document.createElement('h1');
 
-    // Home
-    // Menu
-    // Contact
+    const navBar = document.createElement('ul');
+    const navHome = document.createElement('li');
+    const navMenu = document.createElement('li');
+    const navConact = document.createElement('li');
 
+    const navHomeAnchor = document.createElement('a');
+    const navMenuAnchor = document.createElement('a');
+    const navContactAnchor = document.createElement('a');
+
+    navHomeAnchor.href = '';
+    navMenuAnchor.href = '';
+    navContactAnchor.href = '';
+
+    navHomeAnchor.textContent = 'Home';
+    navMenuAnchor.textContent = 'Menu';
+    navContactAnchor.textContent = 'Contact';
     shopHeader.textContent = 'Cafe Naples';
     shopHeader.classList.add('cafeName');
 
-    header.appendChild(shopHeader)
-    body.insertBefore(header, document.querySelector('#content'))
+    navBar.appendChild(navHome).appendChild(navHomeAnchor);
+    navBar.appendChild(navMenu).appendChild(navMenuAnchor);
+    navBar.appendChild(navConact).appendChild(navContactAnchor);
+
+    header.appendChild(shopHeader);
+    header.appendChild(navBar);
+    body.insertBefore(header, document.querySelector('#content'));
 }
 
 export {createHeader}
